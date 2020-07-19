9d500272c87561b77d35a6cb42f4a3d38dd911e0
kimminja <mandojjang@gmail.com>
2020-07-19 21:35:02 +0900

    Create ArticleService,ArticleServiceImpl,ArticleDao and getList() for fake data return

--- src/main/java/com/bbs/starter/controller/ArticleController.java
+++ src/main/java/com/bbs/starter/controller/ArticleController.java
@@ -1,13 +1,26 @@
 package com.bbs.starter.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bbs.starter.dto.Article;
+import com.bbs.starter.service.ArticleService;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class ArticleController {
+	@Autowired
+	ArticleService articleService;
+
 	@RequestMapping("/article/list")
 	public String showList() {
-		
+		List<Article> list = articleService.getList();
+		log.info("list:" + list);
 		return "article/list";
 	}
 
---
+++ src/main/java/com/bbs/starter/service/ArticleService.java
@@ -0,0 +1,12 @@
+package com.bbs.starter.service;
+
+import java.util.List;
+
+import com.bbs.starter.dto.Article;
+
+
+public interface ArticleService {
+
+	public List<Article> getList();
+
+}
