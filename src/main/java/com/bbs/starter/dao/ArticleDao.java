de73ce606be74155a459f7bad775d9aeb1505def
kimminja <mandojjang@gmail.com>
2020-07-19 23:21:40 +0900

    create ArticleDao

---
+++ src/main/java/com/bbs/starter/dao/ArticleDao.java
@@ -0,0 +1,11 @@
+package com.bbs.starter.dao;
+
+import java.util.List;
+
+import com.bbs.starter.dto.Article;
+
+public interface ArticleDao {
+	public List<Article> getList();
+
+
+}
--- src/main/java/com/bbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/bbs/starter/service/ArticleServiceImpl.java
@@ -1,29 +1,23 @@
 package com.bbs.starter.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bbs.starter.dao.ArticleDao;
 import com.bbs.starter.dto.Article;
 @Service
 public class ArticleServiceImpl implements ArticleService {
 
+	@Autowired
+	ArticleDao articleDao;
+
 	@Override
 	public List<Article> getList() {
 
-		//fake data for test
-		Article article1=new Article(1,"2019-01-01 12:12:12","tittle1","content1");
-		Article article2=new Article(2,"2019-01-01 12:12:12","tittle2","content2");
-		Article article3=new Article(3,"2019-01-01 12:12:12","tittle3","content3");
-
-		List<Article> list = new ArrayList<>();
-
-		list.add(article3);
-		list.add(article2);
-		list.add(article1);
-
-		return list;
+		return articleDao.getList();
 	}
+
 
 }
